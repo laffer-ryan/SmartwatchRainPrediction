440bcae1ebceca589dafea7b8dd0cecd63cb199f
laffer-ryan <lafferty.ryan10@gmail.com>
2023-10-17 15:31:43 +1300

    Initial Commit

---
+++ RainSensorPrediction Watch App/RainSensorPredictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainSensorPredictionApp.swift
+//  RainSensorPrediction Watch App
+//
+//  Created by Ryan Lafferty on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RainSensorPrediction_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
